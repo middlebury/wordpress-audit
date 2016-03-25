dc0e99f9e7a5dda3762c12abf06fdf6eb8af4520
Ian McBride <imcbride@middlebury.edu>
2016-03-25 10:58:42 -0400

    Add a simple Notes form to the Plugin view.

--- src/AppBundle/Controller/PluginsController.php
+++ src/AppBundle/Controller/PluginsController.php
@@ -2,10 +2,16 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Note;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class PluginsController extends Controller
 {
@@ -28,15 +34,38 @@ class PluginsController extends Controller
     /**
      * @Route("/plugins/{pluginName}", name="show_plugin")
      */
-    public function showPlugin($pluginName)
+    public function showPlugin($pluginName, Request $request)
     {
         $plugin = $this->getDoctrine()
             ->getRepository('AppBundle:Plugin')
             ->findOneByName($pluginName);
+            
+        $note = new Note();
+
+        $form = $this->createFormBuilder($note)
+            ->add('author', TextType::class)
+            ->add('date', DateType::class)
+            ->add('note', TextareaType::class)
+            ->add('save', SubmitType::class, array('label' => 'Add Note'))
+            ->getForm();
+            
+        $form->handleRequest($request);
+        
+        if ($form->isSubmitted() && $form->isValid()) {            
+            $plugin->addNote($note);
+            
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($plugin);
+            $em->persist($note);
+            $em->flush();
+
+            return $this->redirectToRoute('show_plugin', array('pluginName' => $pluginName));
+        }
 
         return $this->render('plugin.html.twig', [
             'title' => "WordPress Plugins: " . $pluginName,
             'plugin' => $plugin,
+            'form' => $form->createView(),
         ]);
     }
 }
