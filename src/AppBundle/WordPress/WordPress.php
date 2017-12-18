09e81ba5db770b98204c5e6beb6c7f8d63d5ebf7
Corey Selover <gselover@middlebury.edu>
2017-12-18 14:08:13 -0500

    Revert "Even more log output for debugging"
    This reverts commit 442c194774cbd4cd1e184104ba0c3192718d533b.

--- src/AppBundle/WordPress/WordPress.php
+++ src/AppBundle/WordPress/WordPress.php
@@ -175,6 +175,7 @@ class WordPress
      */
     public function getPlugins()
     {
+        alert('howdy');
         // Will store the data returned by this function
         $plugins =
         // Lists the plugins extant in $this->install_path . $this->plugins_path
@@ -191,7 +192,7 @@ class WordPress
 
         $connection = $this->getConnection();
 
-        alert('asdf');
+        alert('connection successful');
 
         $finder = new Finder();
         $finder->files()
@@ -221,6 +222,7 @@ class WordPress
         $statement->execute();
         $row = $statement->fetch();
         $updates = unserialize($row['meta_value']);
+        alert('_site_transient_update_plugins successful');
 
         // active_sitewide_plugins stores a list of the plugins that are
         // activated on every site on this installation of WordPress and which
@@ -233,6 +235,8 @@ class WordPress
         $statement->execute();
         $row = $statement->fetch();
         $network_activate = unserialize($row['meta_value']);
+        alert('active_sitewide_plugins successful');
+
 
         // pm_auto_activate_list stores a list of the plugins that are
         // activated automatically on all new sites, but which users are
@@ -245,6 +249,8 @@ class WordPress
         $statement->execute();
         $row = $statement->fetch();
         $auto_activate = unserialize($row['meta_value']);
+        alert('pm_auto_activate_list successful');
+
 
         // pm_user_control_list stores a list of the plugins that users
         // are allowed to activate on their sites. Plugins which are not in this
@@ -257,6 +263,8 @@ class WordPress
         $statement->execute();
         $row = $statement->fetch();
         $user_activate = unserialize($row['meta_value']);
+        alert('pm_user_control_list successful');
+
 
         foreach ($installed as $plugin) {
             $record = array();
